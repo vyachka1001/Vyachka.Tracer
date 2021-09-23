43a6512d4c1c0065a139cc3b8afddf651e75d8cd
Viachaslau Viarbitski <viachaslauviarbitski@gmail.com>
2021-09-23 23:35:49 +0300

    Writing result structure

--- TracerImlp/Implementation/TraceResult.cs
+++ TracerImlp/Implementation/TraceResult.cs
@@ -1,7 +1,24 @@
-﻿namespace Tracer.Library.Implementation
-{
-    class TraceResult
-    {
+﻿using System.Collections.Concurrent;
 
+namespace Tracer.Library.Implementation
+{
+    public class TraceResult
+    {
+        private ConcurrentDictionary<int, ThreadTrace> ThreadTraces { get; }
+
+        public TraceResult(ConcurrentDictionary<int, ThreadTrace> traces)
+        {
+            ThreadTraces = traces;
+        }
+
+        public ConcurrentDictionary<int, ThreadTrace> GetThreadTraces()
+        {
+            return ThreadTraces;
+        }
+
+        public ThreadTrace GetThreadTrace(int id)
+        {
+            return ThreadTraces.GetOrAdd(id, new ThreadTrace(id));
+        }
     }
 }
