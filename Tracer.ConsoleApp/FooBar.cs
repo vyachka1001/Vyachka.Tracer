41080b04590be12f1c3b909b688493b8c870fe91
Viachaslau Viarbitski <viachaslauviarbitski@gmail.com>
2021-09-23 23:38:20 +0300

    Creating example classes

---
+++ Tracer.ConsoleApp/FooBar.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading;
+using Tracer.Library.Implementation;
+
+namespace Tracer.ConsoleApp
+{
+    public class Foo
+    {
+        private Bar _bar;
+        private ITracer _tracer;
+
+        internal Foo(ITracer tracer)
+        {
+            _tracer = tracer;
+            _bar = new Bar(_tracer);
+        }
+
+        public void MyMethod()
+        {
+            _tracer.StartTrace();
+            _bar.InnerMethod();
+            _tracer.StopTrace();
+        }
+    }
+
+    public class Bar
+    {
+        private ITracer _tracer;
+
+        internal Bar(ITracer tracer)
+        {
+            _tracer = tracer;
+        }
+
+        public void InnerMethod()
+        {
+            _tracer.StartTrace();
+            Thread.Sleep(100);
+            _tracer.StopTrace();
+        }
+    }
+}
--- Tracer.ConsoleApp/Program.cs
+++ Tracer.ConsoleApp/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Threading;
+using Tracer.Library.Implementation;
 
 namespace Tracer.ConsoleApp
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var program = new Program();
+            Thread thread = new(program.Method);
+            ITracer tracer = new Library.Implementation.Tracer();
+
+            var foo = new Foo(tracer);
+            foo.MyMethod();
+            thread.Start(tracer);
+            thread.Join();
+
+            var result = tracer.GetTraceResult();
+            foreach(var threadTrace in result.GetThreadTraces().Values)
+            {
+                Console.WriteLine($"ThreadId = {threadTrace.ThreadId}; ThreadTime = {threadTrace.ThreadTime}");
+                foreach(var info in threadTrace.MethodsInfo)
+                {
+                    Console.WriteLine($"Class: {info.ClassName}, Method: {info.MethodName}, Time: {info.Time}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public void Method(object obj)
+        {
+            var tracer = (Library.Implementation.Tracer) obj;
+            tracer.StartTrace();
+            Thread.Sleep(100);
+            tracer.StopTrace();
         }
     }
 }
