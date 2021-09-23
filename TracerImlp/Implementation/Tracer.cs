f5d96884e5381a120b48b7efff037201a389e440
Viachaslau Viarbitski <viachaslauviarbitski@gmail.com>
2021-09-23 23:37:18 +0300

    Adding Start and Stop methods

--- TracerImlp/Implementation/Tracer.cs
+++ TracerImlp/Implementation/Tracer.cs
@@ -1,4 +1,8 @@
-﻿namespace Tracer.Library.Implementation
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Tracer.Library.Implementation
 {
     public class Tracer : ITracer
     {
@@ -6,22 +10,38 @@
         
         public Tracer()
         {
-            _traceResult = new TraceResult();
-        }
-
-        public void StartTrace()
-        {
-
-        }
-
-        public void StopTrace()
-        {
-
+            _traceResult = new TraceResult(new ConcurrentDictionary<int, ThreadTrace>());
         }
 
         public TraceResult GetTraceResult()
         {
             return _traceResult;
+        }
+
+        public void StartTrace()
+        {
+            var threadTrace = _traceResult.GetThreadTrace(Thread.CurrentThread.ManagedThreadId);
+
+            var stackTrace = new StackTrace();
+            var path = stackTrace.ToString().Split(new string[] { "\r\n" }, System.StringSplitOptions.None);
+
+            path[0] = "";
+            var className = stackTrace.GetFrames()[1].GetMethod().ReflectedType.Name;
+            var methodName = stackTrace.GetFrames()[1].GetMethod().Name;
+
+            threadTrace.AddMethod(methodName, className, string.Join("", path));
+        }
+
+        public void StopTrace()
+        {
+            var threadTrace = _traceResult.GetThreadTrace(Thread.CurrentThread.ManagedThreadId);
+
+            var stackTrace = new StackTrace();
+            var path = stackTrace.ToString().Split(new string[] { "\r\n" }, System.StringSplitOptions.None);
+
+            path[0] = "";
+
+            threadTrace.DeleteMethod(string.Join("", path));
         }
     }
 }
